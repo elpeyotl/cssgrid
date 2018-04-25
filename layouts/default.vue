462cd6f92efb6042ca19d7e019c6f3623ec105e7
Chris Perez <cp@reachpartners.ch>
2018-04-25 18:20:36 +0200

    first

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="main-wrapper no-grid">
+    <nuxt/>
+  </div>
+</template>
+
+<style lang="scss">
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="container grid text-center">
+    <div class="bg-primary header">HEADER</div>
+      <div class="bg-primary sidebar">
+        <div class="sidebar-inner  bg-secondary">Sidebar</div>
+      </div>
+      <div class="bg-primary content">
+        <div class="sidebar-inner bg-secondary content-inner">Content</div>
+      </div>
+    <footer class="bg-third">FOOTER</footer>
+  </section>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+@import '~assets/css/main.scss';
+.header {
+  margin-bottom: 20px;
+  height: 100px;
+}
+
+.sidebar,.content {
+  height: 100%;
+  padding: 20px;
+}
+.sidebar-inner, .content-inner {
+  height: 400px;
+}
+</style>
